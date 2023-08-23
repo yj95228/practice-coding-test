2d1221382fe082b266d5c58fde82bdd945783014
yj95228 <kei02119@naver.com>
2023-08-23 20:59:42 +0900

    8/23(수)

---
+++ python/baekjoon/[3190] 뱀.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/3190
+import sys
+sys.stdin = open('input.txt','r')
+input = sys.stdin.readline
+
+N = int(input())
+matrix = [[0]*N for _ in range(N)]
+K = int(input())
+for _ in range(K):
+    x, y = map(int, input().split())
+    matrix[x-1][y-1] = 1
+L = int(input())
+info = {}
+for _ in range(L):
+    cnt, dr = input().split()
+    info[int(cnt)] = dr
+answer, r, c, d = 0, 0, 0, 0
+snake = [(r,c)]
+direction = [(0,1),(1,0),(0,-1),(-1,0)]
+
+while True:
+    dx, dy = direction[d]
+    r, c = snake[-1]
+    answer += 1
+    if 0 <= r+dx < N and 0 <= c+dy < N and (r+dx, c+dy) not in snake:
+        snake.append((r+dx, c+dy))
+        if matrix[r+dx][c+dy] == 0: snake.pop(0)
+        else: matrix[r+dx][c+dy] = 0
+        if answer in info:
+            if info[answer] == 'L': d = (d-1)%4
+            else: d = (d+1)%4
+    else: break
+print(answer)
